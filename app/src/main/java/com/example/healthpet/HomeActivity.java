862eb743d5e70b147d0a3358f73919046087baac
cookies1202 <manuel.haas@edu.fh-joanneum.at>
2025-05-19 20:52:11 +0200

    add koala_eating

--- app/src/main/java/com/example/healthpet/HomeActivity.java
+++ app/src/main/java/com/example/healthpet/HomeActivity.java
@@ -3,12 +3,15 @@ package com.example.healthpet;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -26,6 +29,8 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         welcomeTextView = findViewById(R.id.welcomeTextView);
+
+
 
         stepGoalButton = findViewById(R.id.stepGoalButton);
         waterGoalButton = findViewById(R.id.waterGoalButton);
@@ -67,6 +72,13 @@ public class HomeActivity extends AppCompatActivity {
         breathingTaskButton.setOnClickListener(v -> {
             Intent intent = new Intent(HomeActivity.this, BreathingTaskActivity.class);
             startActivity(intent);
+        });
+
+        Button showKoalaButton = findViewById(R.id.showKoalaButton);
+        LottieAnimationView koalaView = findViewById(R.id.koala_eating);
+
+        showKoalaButton.setOnClickListener(v -> {
+            koalaView.setVisibility(View.VISIBLE);
         });
     }
 }
